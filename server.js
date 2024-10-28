51866e7eee7fade1c88c88284226829c9ee5edc0
Luis Ignacio Gonzalez Silva <luis_gosi@outlook.com>
2024-10-27 19:23:15 -0600

    Rework de servicio

--- server.js
+++ server.js
@@ -1,91 +1,69 @@
-const cors = require('cors');
-const superTokens = require("supertokens-node");
-const Session = require("supertokens-node/recipe/session");
-const EmailPassword = require("supertokens-node/recipe/emailpassword");
-const Dashboard = require("supertokens-node/recipe/dashboard");
-const { errorHandler, middleware } = require('supertokens-node/framework/express');
-const { signUpClicked, checkEmail } = require('./api/authFunctions');
-require('dotenv').config();
 const express = require('express');
+const { initializeApp } = require('firebase/app');
+const { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } = require('firebase/auth');
 
-// Inicializacion de la aplicacion
+// Inicialización de la aplicación
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Puerto de la aplicacion
+// Puerto de la aplicación
 const port = process.env.PORT || 8080;
 
-// Configuracion de SuperTokens
-superTokens.init({
-    framework: "express",
-    supertokens: {
-        connectionURI: process.env.CONNECTION_URL,
-        apiKey: process.env.API_KEY
-    },
-    appInfo: {
-        appName: "ejemploMicroservicios",
-        apiDomain: "https://microservicioauth.onrender.com",
-        websiteDomain: "https://frontend-microservicios.vercel.app/",
-        apiBasePath: "/authentication"
-    },
-    recipeList: [
-        EmailPassword.init(),
-        Session.init(),
-        Dashboard.init({
-            admins: [
-                "luis_gosi@outlook.com"
-            ]
-        })
-    ]
+// Configuración de Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyCW0LjYlWCadlfAn34m25zMiZuQIYj6ln4",
+    authDomain: "microserviceauth-686eb.firebaseapp.com",
+    projectId: "microserviceauth-686eb",
+    storageBucket: "microserviceauth-686eb.appspot.com",
+    messagingSenderId: "825812357836",
+    appId: "1:825812357836:web:874bbbe4fef9711f85e869"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const auth = getAuth(firebaseApp);
+
+//! Rutas -----------------------------------------------------------------------
+
+app.post("/auth/register", async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+        const token = await userCredential.user.getIdToken(); 
+        res.json({
+            message: 'User registered successfully',
+            token: token,
+            user: {
+                email: userCredential.user.email,
+                uid: userCredential.user.uid
+            }
+        });
+    } catch (error) {
+        res.status(400).json({
+            message: 'Error registering user',
+            error: error.message
+        });
+    }
 });
 
-// Configuracion de CORS
-app.use(cors({
-    origin: '*', // O el dominio permitido
-    allowedHeaders: ["content-type", "authorization", ...superTokens.getAllCORSHeaders()],
-    credentials: true
-}));
-
-
-
-// Middleware de SuperTokens
-app.use(middleware());
-
-//!Rutas -----------------------------------------------------------------------------
-
-// Ruta de SignUp
-// app.post("/authentication/signUp", async (req, res) => {
-//     console.log(req.body);
-//     const { email, password } = req.body;
-//     console.log(email);
-//     console.log(password);
-//     if (email === "" || password === "") {
-//         return res.status(400).send("Email or password cannot be empty");
-//     }
-//     if (await checkEmail(email) === 1) {
-//         return res.status(400).send("Email already in use");
-//     }
-//     let signUpResponse = await signUpClicked(email, password);
-//     console.log("Si paso por la funcion de signUp");
-//     if (signUpResponse.error) {
-//         res.status(500).json({message: "Error al registrar la cita"});    }
-//     else {
-//         res.status(200).json({ message: "Nueva cita registrada" });
-//     }
-// });
-
-// Ruta de SignIn
-app.post("/authentication/signIn", async (req, res) => {
+app.post("/auth/login", async (req, res) => {
+    const { email, password } = req.body;
     try {
-        let signInResponse = await EmailPassword.signIn(req.body.email, req.body.password);
-        if (signInResponse.error) {
-            return res.status(400).send(signInResponse.error);
-        } else {
-            return res.status(200).send("Sign In successful");
-        }
-    } catch (err) {
-        res.status(400).send("Error");
+        const userCredential = await signInWithEmailAndPassword(auth, email, password);
+        const token = await userCredential.user.getIdToken();
+        res.json({
+            message: 'User logged in successfully',
+            token: token,
+            user: {
+                email: userCredential.user.email,
+                uid: userCredential.user.uid
+            }
+        });
+    } catch (error) {
+        res.status(400).json({
+            message: 'Error logging in',
+            error: error.message
+        });
     }
 });
 
@@ -97,10 +75,6 @@ app.get("/", (req, res) => {
 // Ruta de verificación
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
-    console.log(process.env.CONNECTION_URL);
 });
 
-//!-----------------------------------------------------------------------------------
-
-// Error handler
-app.use(errorHandler());
+//!--------------------------------------------------------------------------------
